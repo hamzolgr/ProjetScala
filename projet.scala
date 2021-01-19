040e96e952d1adc868d81f1812c558bd018ec1f6
hamzolgr <66445746+hamzolgr@users.noreply.github.com>
2021-01-19 10:14:58 +0000

    Update projet.scala

--- projet.scala
+++ projet.scala
@@ -9,5 +9,25 @@ object MyApp extends SimpleSwingApplicationþ{
     def top =  new MainFrame {
     title = "MyApplicationþ"
       contents = myButton
+      
+      class Professeur(var idProfesseur: String, var nomProfesseur: String, var prenomProf: String){
+        def idProf = idProfesseur
+        def nomPro = nomProfesseur
+        def prenomProf = prenomProfesseur
+        
+      class  Etudiant(var idstudent: String, var nomstudent: String, var prenomstudent: String, var niveau: String){
+        def idEtudiant = idstudent
+        def nomEtudiant = nomstudent
+        def prenomEtudiant = prenomstudent
+        def niveauEtude = niveau
+        
+      class Salle(var numeroSalle: Int, var libelleSalle: String){
+        def numSalle = numeroSalle
+        def libelle_Salle = libelleSalle
+      class  Matiere(var codeMatiere: String, var libelleMatiere: String, var coeffMatiere: Int){
+        def idMatiere = codeMatiere
+        def lib_Matiere = libelleMatiere
+        def coeff_Matiere = coeffMatiere
+
   }
 }
